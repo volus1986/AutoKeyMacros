21eed789ab72d61f03270d7f0675f58d61250d02
Volodymyr Ustilkin <71969612+volus1986@users.noreply.github.com>
2021-01-19 12:29:15 +0200

    Add files via upload

---
+++ clicker.py
@@ -0,0 +1,58 @@
+# v20210119
+import time
+
+# number type (integer/long/float/double) -- time to delay between keys
+# string type keys shoult be press
+# example MACRO_KEYS = [2.7, "1", "2", 1, "z", ""]
+# set delay between keys 2.7 seconds,2) push key 1, delay, push key 2, set delay between keys 1 second, push key z, wait (no key press), restart cycle
+
+# MB? = mouse.click_relative_self(x, y, button number). Can use at keyboard key position
+MB1 = [0, 0, 1]
+MB2 = [0, 0, 2]
+
+# number type (integer/float) -- time to delay between keys
+# string type keys shoult be press
+MACRO_KEYS = [2.7, "1", "2", "1", "3", "3", "6", "1", "z", "1", ""]
+
+
+# configure
+MAX_CYCLE_COUNT = 10000	# max cycles limit count
+MAX_WORK_TIME = 12 * 60 * 60	# maximum work time limit in seconds
+STOP_KEY = '<escape>'		# key to break macros
+DELAY_BEFORE_SCRIPT_RUN = 0.5	# delay before script run in seconds
+
+############################################################################################
+break_flag = False
+def main_process():
+    delay = 0.1     # default delay between keys
+    i = 0           # limit cycles counter 
+    while i < MAX_CYCLE_COUNT:
+        for key in MACRO_KEYS:
+            if type(key) == int or type(key) == float:
+                delay = key
+                continue
+
+            elif key == MB1 or key == MB2:
+                mouse.click_relative_self(key[0], key[1], key[2]);
+            
+            elif type(key) == str:
+                if key != "":
+                    keyboard.send_keys(key)
+                    
+            time.sleep(delay)
+            
+            if break_flag:
+                break         
+            
+        i = i + 1
+
+        if break_flag:
+            break
+   
+time.sleep(DELAY_BEFORE_SCRIPT_RUN)   
+thr = threading.Thread(target = main_process, args = ())
+thr.start()
+
+keyboard.wait_for_keypress(STOP_KEY, [], MAX_WORK_TIME)
+break_flag = True
+thr.join()
